1b184eb09245292b41fe8c61780c15a9c5ab325a
Egor <y.zaporozhan_fit_5_23_b_d@knute.edu.ua>
2026-09-15 13:30:07 +0300

    fix: gallery markup and styles

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,5 @@
 import '../css/styles.css';
-import '../css/gallery.css';
+// import '../css/gallery.css';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
